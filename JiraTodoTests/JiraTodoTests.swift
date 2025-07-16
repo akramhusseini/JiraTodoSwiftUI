2cd151552e68f2bfa4fb8a4e6687bbf362a81225
akrammotory <93082320+akrammotory@users.noreply.github.com>
2025-07-16 11:41:33 +0300

    Initial Commit

---
+++ JiraTodoTests/JiraTodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  JiraTodoTests.swift
+//  JiraTodoTests
+//
+//  Created by Akram Husseini on 16/07/2025.
+//
+
+import Testing
+@testable import JiraTodo
+
+struct JiraTodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ JiraTodoUITests/JiraTodoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  JiraTodoUITestsLaunchTests.swift
+//  JiraTodoUITests
+//
+//  Created by Akram Husseini on 16/07/2025.
+//
+
+import XCTest
+
+final class JiraTodoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
